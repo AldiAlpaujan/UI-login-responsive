a721fc19a9ee0421455064dc63a86ce43f7b87fe
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2021-12-07 14:45:34 +0700

    membuat layar menjadi selalu porttrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
